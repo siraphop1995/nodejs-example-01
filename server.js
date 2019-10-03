bb7c4bec27515eb168cad890b843e471f7361b96
siraphop1995 <siraphop.a95@gmail.com>
2019-10-03 16:33:46 +0700

    edit server.js

--- server.js
+++ server.js
@@ -6,28 +6,30 @@ const cors = require('cors');
 app = express();
 port = process.env.PORT || 3000;
 
-User = require('./api/models/userListModel');
-
-mongoose.Promise = global.Promise;
-mongoose.set('useCreateIndex', true);
+mongoose.Promise = require('bluebird');
+const mongooseConfig = {
+  useNewUrlParser: true,
+  useCreateIndex: true
+};
 mongoose.connect(
   'mongodb+srv://admin:admin@cluster0-yejh3.gcp.mongodb.net/test?retryWrites=true&w=majority',
-  { useNewUrlParser: true },
+  mongooseConfig,
   function(error) {
     if (error) throw error;
     console.log('Successfully connected');
   }
 );
 
+//Express middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-const userRouter = require('./api/routes/userRouter');
-
-app.use(userRouter);
-
 app.use(errorHandler);
+
+//Model and router
+User = require('./api/models/userListModel');
+const userRouter = require('./api/routes/userRouter');
+app.use(userRouter);
 
 function errorHandler(err, req, res, next) {
   console.error(err);
@@ -37,6 +39,7 @@ function errorHandler(err, req, res, next) {
   res.status(500).send(newError);
 }
 
-app.listen(port);
-
-console.log('Server started on : ' + port);
+//Listen port
+app.listen(port, () => {
+  console.log('Start listen on port: ' + port);
+});
